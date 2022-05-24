fb4767f657885856535374bfc084e03eb7dab2b3
ESDUNCAN <d.eric@rogers.com>
2022-05-24 21:45:37 +0000

    added functioning counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $("#tweet-text").on("input", onChange);
+});
+
+const onChange = function () {
+  const $textArea = $(this)
+  const text = $textArea.val()
+  const length = text.length
+  console.log(length, text);
+
+  const $counter = $(".counter")
+  const remaining = (140 - length)
+  $counter.text(remaining)
+  if (remaining < 0) {
+    $counter.addClass("red")
+  } else if (remaining >= 0) {
+    $counter.removeClass("red")
+  }
+}
+
+
+
+
+
